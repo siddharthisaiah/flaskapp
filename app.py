e70260a5fc3fdab0017d076325102edfdd3f77b1
Siddharth <siddharthisaiah@gmail.com>
2017-11-02 14:41:19 +0530

    part 2 25.17

---
+++ app.py
@@ -0,0 +1,66 @@
+from flask import Flask, render_template, flash, redirect, url_for, session, request, logging
+from data import Articles
+from flask_mysqldb import MySQL
+from wtforms import Form, StringField, TextAreaField, PasswordField, validators
+from passlib.hash import sha256_crypt
+
+app = Flask(__name__)
+app.debug = True
+
+# Config MySQL
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'toor'
+app.config['MYSQL_DB'] = 'myflaskapp'
+app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
+# init MYSQL
+mysql = MySQL(app)
+
+Articles = Articles()
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
+@app.route('/articles')
+def articles():
+    return render_template('articles.html', articles=Articles)
+
+
+@app.route('/article/<string:id>/')
+def article(id):
+    return render_template('article.html', id=id)
+
+
+class RegisterForm(Form):
+    name = StringField('Name', [validators.Length(min=1, max=50)])
+    username = StringField('Username', [validators.Length(min=4, max=25)])
+    email = StringField('Email', [validators.Length(min=6, max=50)])
+    password = PasswordField('Password', [
+        validators.data_required(),
+        validators.EqualTo('confirm', message="Passwords do not match")
+    ])
+    confirm = PasswordField('Confirm Password')
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegisterForm(request.form)
+
+    if request.method == 'POST' and form.validate():
+        name = form.name.data
+        email = form.email.data
+        username = form.username.data
+        password = form.password.data
+    return render_template('register.html', form=form)
+
+
+if __name__ == "__main__":
+    app.run()
---
+++ data.py
@@ -0,0 +1,26 @@
+def Articles():
+    articles = [
+        {
+            'id': 1,
+            'title': 'Article One',
+            'body': 'lorem ipsum dolor set amit lorem ipsum dolor set amit lorem ipsum dolor set amit lorem ipsum dolor set amit',
+            'author': 'Brad Traversty',
+            'create_date': '04-05-2017'
+        },
+        {
+            'id': 2,
+            'title': 'Article Two',
+            'body': 'lorem ipsum dolor set amit lorem ipsum dolor set amit lorem ipsum dolor set amit lorem ipsum dolor set amit',
+            'author': 'John Doe',
+            'create_date': '05-05-2017'
+        },
+        {
+            'id': 3,
+            'title': 'Article Three',
+            'body': 'lorem ipsum dolor set amit lorem ipsum dolor set amit lorem ipsum dolor set amit lorem ipsum dolor set amit',
+            'author': 'Bugs Bunny',
+            'create_date': '06-05-2017'
+        }
+    ]
+
+    return articles
